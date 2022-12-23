bc5cbb093c65de40a9a1eeaa49e79e45d5ae43d3
Martin Janiczek <martin@janiczek.cz>
2022-12-23 21:10:45 +0100

    Show the table with the items (doesn't run tests yet)

--- runner.py
+++ runner.py
@@ -1,50 +1,81 @@
 from textual.app import App, ComposeResult
 from textual.reactive import reactive
-from textual.widgets import DataTable, Footer
-from typing import List
+from textual.widgets import DataTable, Footer, Static
+from typing import List, Dict
+import os
 import sys
+
+class Summary(Static):
+    def set_progress(self, done: int, total: int, passed: int) -> None:
+        self.update(self.format(done, total, passed))
+        if done == total:
+            if passed == total:
+                self.set_class("passed")
+            else:
+                self.set_class("failed")
+                
+    def format(self, done: int, total: int, passed: int) -> str:
+        done_pct   = 0 if total == 0 else done*1.0/total
+        passed_pct = 0 if total == 0 else passed*1.0/total
+        return f"{done}/{total} tests done ({done_pct:.2f}%), {passed} passed ({passed_pct:.2f}%)"
 
 class Runner(App):
 
-    BINDINGS = [("q", "quit", "Quit")]
+    BINDINGS = [
+        ("q",     "quit",          "Quit"),
+        ("space", "filter_passed", "Show/hide passed tests"),
+    ]
 
-    def __init__(self, tests_dir: str="end-to-end-tests") -> None:
-        self.tests_dir = tests_dir
-        super().__init__()
+    CSS_PATH = "app.css"
+
+    tests_dir = "end-to-end-tests"
+    tests     = reactive({})
+    tests_idx = reactive({})
+    filter_passed = reactive(False)
+    done   = reactive(0)
+    total  = reactive(0)
+    passed = reactive(0)
+
+    def get_key_display(self, key: str) -> str:
+        if key == "space": return "Space"
+        return super().get_key_display(key)
 
     def compose(self) -> ComposeResult:
+        self.summary = Summary()
+        yield self.summary
         yield DataTable()
         yield Footer()
 
     def on_mount(self) -> None:
+        self.summary.set_progress(0,0,0)
+
+        self.tests = self.find_tests()
+        self.tests_idx = {name:i for i,name in enumerate(sorted(self.tests.keys()))}
+        self.run_tests()
+
         self.table = self.query_one(DataTable)
-        tests = self.find_tests()
-        self.table.add_columns("Test","Result")
-        self.table.add_rows(tests)
-        self.set_timer(2,self.change_one_test)
+        self.table.add_columns("Test","Status")
+        self.table.add_rows(sorted([[k,v] for k,v in self.tests.items()]))
 
-    def find_tests(self) -> List[str]:
-        print("TODO find tests")
-        return [
-            ["foo test 1","Pending"],
-            ["bar 2 test","OK"],
-        ]
+    def set_test_status(self, test_name: str, new_status: str) -> None:
+        idx = self.tests_idx[test_name]
+        self.table.data[idx][1] = new_status
+        self.table.refresh_cell(idx,1)
 
-    def change_one_test(self) -> None:
-        print("x")
-        self.table.data[0][1] = "OK"
-        self.table.refresh_cell(0,1)
+        # TODO: Expensive. Shouldn't be needed but currently is,
+        # at least as of textual==0.8.0
         self.table._clear_caches()
+
+    def action_filter_passed(self) -> None:
+        self.filter_passed = not self.filter_passed
+
+    def find_tests(self) -> Dict[str, str]:
+        return {os.path.basename(d[0]):"Not started" for d in os.walk(self.tests_dir)}
+
+    def run_tests(self) -> None:
+        pass
 
 
 if __name__ == "__main__":
-    argc = len(sys.argv)
-    tests_dir = None
-    if argc == 2:
-        tests_dir = sys.argv[1]
-    elif argc > 2:
-        print("Usage: python runner.py [TESTS_DIR]")
-        sys.exit(2)
-
-    app = Runner(tests_dir)
+    app = Runner()
     app.run()
